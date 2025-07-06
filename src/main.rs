1000a93b33557d5d8b521ca239ec66a7052198c9
Dillon Beliveau <dillon@dgb.sh>
2025-07-06 10:45:25 -0700

    support printing all trains, support station codes longer than 3 chars

--- src/main.rs
+++ src/main.rs
@@ -39,21 +39,27 @@ fn display_train(train: &Train) {
     let next_station = &train.event_code;
 
     println!(
-        "Train ID {} from {} to {}",
-        train.train_id, train.origin_code, train.destination_code
+        "Train ID {} from {} to {} \"{}\"",
+        train.train_id, train.origin_code, train.destination_code, train.route_name
     );
+
     let next_station = train
         .stations
         .iter()
         .find(|s| s.code == *next_station)
         .unwrap();
+    println!(
+        "Next station: {} ({})",
+        next_station.name, next_station.code
+    );
 
     let prev_station = train
         .stations
         .iter()
         .take_while(|s| s.code != next_station.code)
         .last()
-        .unwrap();
+        .map(|s| s.code.clone())
+        .unwrap_or("".to_string());
 
     println!(
         "\tNext Station: {} ({})",
@@ -62,33 +68,48 @@ fn display_train(train: &Train) {
 
     {
         println!("\t\tScheduled: {}", next_station.schedule_arrival);
-        println!("\t\tActual: {:?}", next_station.arrival.unwrap());
-        let arrival_delta = next_station.arrival.unwrap() - next_station.schedule_arrival;
-        println!("\t\t{}", arrival_delta_to_human_string(arrival_delta));
+        println!(
+            "\t\tActual: {:?}",
+            next_station
+                .arrival
+                .map(|a| a.to_string())
+                .unwrap_or("Unknown".to_string())
+        );
+        let arrival_delta = next_station
+            .arrival
+            .map(|arrival| arrival - next_station.schedule_arrival)
+            .map(|delta| arrival_delta_to_human_string(delta))
+            .unwrap_or("Unknown".to_string());
+        println!("\t\t{}", arrival_delta);
     }
+
+    let longest_station_code_len = train
+        .stations
+        .iter()
+        .map(|s| s.code.len())
+        .max()
+        .unwrap_or(0);
+    let spaces = " ".repeat(longest_station_code_len + 1);
 
     let train_icon = "󰣄";
     let lines = train
         .stations
         .iter()
         .flat_map(|station| {
-            assert_eq!(
-                station.code.len(),
-                3,
-                "Station code should be 3 characters long"
-            );
+            let pipes = "|".repeat((longest_station_code_len + 1) - station.code.len());
+            let station_line = format!("{}{}-", station.code, pipes);
 
-            let station_line = format!("{}|-", station.code);
-            let train_icon_line = if station.code == prev_station.code {
-                format!("    {}", train_icon)
+            let spacer_line = format!("{}-", spaces);
+            let train_icon_line = if station.code == prev_station {
+                format!("{}{}", spaces, train_icon)
             } else {
-                "    -".to_string()
+                spacer_line.clone()
             };
 
             if station.code == train.destination_code {
                 vec![station_line]
             } else {
-                vec![station_line, "    -".to_string(), train_icon_line]
+                vec![station_line, spacer_line, train_icon_line]
             }
         })
         .collect::<Vec<String>>();
@@ -102,18 +123,7 @@ fn display_train(train: &Train) {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = std::env::args().collect::<Vec<String>>();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <train_id>", args[0]);
-        eprintln!(
-            "Example: '7-27' for the train 7 that left the station on the 27th, or 7-4 for train 7 that left the station on the 4th of the month."
-        );
-        return Ok(());
-    }
-
-    let train_id = &args[1];
+async fn display_train_id(train_id: &str) -> Result<(), Box<dyn std::error::Error>> {
     let train_id_parts: Vec<&str> = train_id.split('-').collect();
     if train_id_parts.len() != 2 {
         eprintln!("Invalid train ID format. Expected format: <train_number>-<date>");
@@ -135,4 +145,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let trains = maybe_trains.unwrap();
     display_train(trains.first().unwrap());
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        eprintln!("Usage: {} [<train_id>|<train_number>|all]", args[0]);
+        eprintln!(
+            "Train ID format: '7-27' for the train 7 that left the station on the 27th, or 7-4 for train 7 that left the station on the 4th of the month."
+        );
+        return Ok(());
+    }
+
+    let train_id = &args[1];
+    if train_id.contains("-") {
+        display_train_id(train_id).await
+    } else if train_id == "all" {
+        let client = Client::new();
+        let trains = client.trains().await?;
+        trains
+            .values()
+            .flat_map(|trains| trains)
+            .filter(|train| train.provider == "Amtrak")
+            .for_each(|train| {
+                display_train(&train);
+                println!();
+            });
+        Ok(())
+    } else {
+        eprintln!("Invalid train ID format. Expected format: <train_number>-<date> or 'all'");
+        Ok(())
+    }
 }
