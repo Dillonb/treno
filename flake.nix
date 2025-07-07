d9a6a3193538eb4e24773323ba1fe6879fd0ea40
Dillon Beliveau <dillon@dgb.sh>
2025-07-06 21:43:24 -0700

    Flake working on Linux, format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,39 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShell {
-          buildInputs = with pkgs.${system}; [ cargo rustc rust-analyzer rustfmt lldb bacon cargo-nextest ];
+          buildInputs =
+            with pkgs.${system};
+            let
+              pkgs_linux = lib.optionals stdenv.isLinux [
+                pkg-config
+                openssl
+              ];
+            in
+            [
+              cargo
+              rustc
+              rust-analyzer
+              rustfmt
+              lldb
+              bacon
+              cargo-nextest
+            ]
+            ++ pkgs_linux;
         };
       });
     };
